4c770f6c9e2694d727d0c26832c4553f51f80743
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-25 14:34:16 +0100

    Add JSDoc to the Navbar

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,5 +1,13 @@
+/**
+ * @file This is the navigation section of the web app.
+ *
+ * @author Stanisław Gregor <gregor.stanislaw@hotmail.com>
+ */
+
+// JS imports
 import React from "react";
 
+// SCSS imports
 import "./_Navbar.scss";
 
 const Navbar = () => <nav className="Navbar">Navbar</nav>;
