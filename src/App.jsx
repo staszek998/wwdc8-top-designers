f948ed5ee098dc8c38ed29c8cc090c4464438f6c
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-25 14:33:19 +0100

    Add JSDoc domment to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React, { Component } from "react";
+/**
+ * @file This is the main file of the web app. It is used to import and control the inner components.
+ *
+ * @author Stanisław Gregor <gregor.stanislaw@hotmail.com>
+ */
 
-import "./App.scss";
+// JS imports
+import React, { Component } from "react";
 import Navbar from "./sections/Navbar/Navbar";
+
+// SCSS imports
+import "./App.scss";
 
 class App extends Component {
   render() {
