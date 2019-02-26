941326575d30d626bde8f6267c69e6072754a159
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-26 11:37:43 +0100

    Create the basic HTML markup

--- src/sections/Circles/Circles.jsx
+++ src/sections/Circles/Circles.jsx
@@ -10,6 +10,46 @@ import React from "react";
 // SCSS imports
 import "./_Circles.scss";
 
-const Circles = () => <section className="Circles text-center">Circles</section>;
+const Circles = () => (
+  <section className="Circles text-center border">
+    <div className="container">
+      <div className="row py-5">
+        <div className="col-12 col-md-4">
+          <div className="d-flex flex-column justify-content-start align-items-center">
+            <div className="circle">circle</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+              itaque aspernatur explicabo! Accusantium suscipit dolorum
+              assumenda eaque harum quo id aliquam. Perspiciatis, quod corrupti
+              ab consectetur esse itaque nesciunt molestiae.
+            </p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4">
+          <div className="d-flex flex-column justify-content-start align-items-center">
+            <div className="circle">circle</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+              itaque aspernatur explicabo! Accusantium suscipit dolorum
+              assumenda eaque harum quo id aliquam. Perspiciatis, quod corrupti
+              ab consectetur esse itaque nesciunt molestiae.
+            </p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4">
+          <div className="d-flex flex-column justify-content-start align-items-center">
+            <div className="circle">circle</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+              itaque aspernatur explicabo! Accusantium suscipit dolorum
+              assumenda eaque harum quo id aliquam. Perspiciatis, quod corrupti
+              ab consectetur esse itaque nesciunt molestiae.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default Circles;
