11f74f31bce7eb6baba81f6b0ccb2c03ec71f9c8
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-26 09:15:30 +0100

    Make the Navbar positionad absolutely within the window

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,39 @@ class App extends Component {
     return (
       <div>
         <Navbar />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum,
+        deleniti quo quibusdam pariatur a optio tempore assumenda sed, ea fuga
+        quis eum nisi. Odit, qui recusandae esse molestias ipsum quam? Ipsa
+        explicabo, iusto ipsum nisi quam dolores facere. Asperiores ullam
+        consectetur illum eligendi dolorem culpa, tempora nisi cumque. Quos
+        autem, quisquam excepturi asperiores quas adipisci quam cum provident
+        alias id. Nesciunt odio praesentium eaque perspiciatis numquam dolore
+        nostrum similique debitis, dicta saepe nobis dolorem eligendi iusto
+        esse, fugiat voluptatem, quibusdam unde tempore id soluta provident cum
+        tenetur accusantium? Perferendis, rerum! Officiis velit laborum ex
+        harum. Animi, placeat officia! At, recusandae repellendus. Esse, odio
+        ab? Nemo et id quam dicta dolore suscipit eum totam tempore labore. Aut
+        porro explicabo eaque blanditiis. Ea, ut exercitationem magnam
+        veritatis, laboriosam quidem obcaecati corporis enim laborum rem ducimus
+        consequuntur. Magni animi veniam doloribus, illo reprehenderit eveniet
+        velit atque exercitationem voluptatem, unde officiis, possimus ea nobis.
+        Aliquam eos hic adipisci ut praesentium, illum rem reprehenderit placeat
+        quidem culpa explicabo temporibus quisquam neque? Aliquid, fugiat
+        delectus! Impedit inventore cum ratione ducimus sunt! Ullam dolores
+        molestiae blanditiis iste? Iusto esse repellat quidem doloremque magnam
+        reprehenderit nihil iure vel! Iusto, dignissimos quae sint vel veritatis
+        pariatur quasi, nobis enim suscipit excepturi odio et repellendus qui
+        maxime magnam eveniet tempore? Tempore accusantium esse sint, doloribus
+        laboriosam perspiciatis. Maxime molestias, deleniti pariatur nulla saepe
+        repellat dolores veniam culpa voluptate illum. Harum veritatis non
+        maxime ducimus. Aspernatur impedit odit optio obcaecati necessitatibus.
+        Tempora, quisquam quam sapiente, eaque voluptatum recusandae placeat
+        temporibus saepe velit, omnis possimus corrupti dolorum praesentium.
+        Amet, labore iure earum, similique ab quo dolores culpa totam eligendi
+        consectetur tempora inventore. Laborum labore voluptatibus possimus
+        error voluptatum. Perferendis neque laudantium numquam velit, accusamus
+        iste. Atque porro, eligendi tempore molestiae sed non, iusto dolor
+        excepturi expedita, soluta nostrum sit doloribus in ea.
         <Hero />
         <main>
           <Circles />
--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -22,60 +22,56 @@ class Navbar extends Component {
   };
 
   render() {
-    return (
-      <nav className="Navbar border">
-        <div className="container">
-          {window.innerWidth <= 768 ? (
-            // Mobile
-            <div className="Navbar--mobile row">
-              {/* DSGN logotype */}
-              <div className="col-6 d-flex justify-content-start align-items-center">
-                <a href="#" className="Navbar__logotype">
-                  DSGN
-                </a>
-              </div>
+    return window.innerWidth <= 768 ? (
+      // Mobile
+      <nav className="Navbar--mobile row">
+        {/* DSGN logotype */}
+        <div className="col-6 d-flex justify-content-start align-items-center">
+          <a href="#" className="Navbar__logotype">
+            DSGN
+          </a>
+        </div>
 
-              {/* Hamburger button / menu trigger */}
-              <div className="col-6 d-flex justify-content-end align-items-center">
-                <button
-                  className="Navbar__menu-trigger"
-                  onClick={this.handleHamburgerClick}
-                >
-                  MENU
-                </button>
-              </div>
+        {/* Hamburger button / menu trigger */}
+        <div className="col-6 d-flex justify-content-end align-items-center">
+          <button
+            className="Navbar__menu-trigger"
+            onClick={this.handleHamburgerClick}
+          >
+            MENU
+          </button>
+        </div>
 
-              {/* Menu content */}
-              <div
-                className={`Navbar__menu-content col-12 ${
-                  this.state.isMenuExpanded ? "expanded" : null
-                }`}
-              >
-                <ul className="d-flex flex-column justify-content-start align-items-stretch">
-                  <li>
-                    <a href="#about-us">About us</a>
-                  </li>
-                  <li>
-                    <a href="#our-team">Our team</a>
-                  </li>
-                  <li>
-                    <a href="#portfolio">Portolio</a>
-                  </li>
-                  <li>
-                    <a href="#brands">Brands</a>
-                  </li>
-                  <li>
-                    <a href="#contact">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          ) : (
-            // Desktop
-            <div className="Navbar--desktop row">Navbar--desktop</div>
-          )}
+        {/* Menu content */}
+        <div
+          className={`Navbar__menu-content col-12 ${
+            this.state.isMenuExpanded ? "expanded" : ""
+          }`}
+        >
+          <ul className="d-flex flex-column justify-content-start align-items-stretch">
+            <li>
+              <a href="#about-us">About us</a>
+            </li>
+            <li>
+              <a href="#our-team">Our team</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portolio</a>
+            </li>
+            <li>
+              <a href="#brands">Brands</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
         </div>
       </nav>
+    ) : (
+      // Desktop
+      <div className="Navbar--desktop row">
+        <nav>Navbar--desktop</nav>
+      </div>
     );
   }
 }
