eca28d14d6825adc93d4cb93b60755cac215d2d3
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-27 11:43:40 +0100

    Create the files for the CircleProgress component

---
+++ src/components/CircleProgress/CircleProgress.jsx
@@ -0,0 +1,22 @@
+/**
+ * @file This component will render the circle progressbar using the ProgressBar.js library.
+ *
+ * @author Stanisław Gregor <gregor.stanislaw@hotmail.com>
+ * @author Kimmo Brunfeldt (ProgressBar.js) <https://github.com/kimmobrunfeldt>
+ */
+
+// JS imports
+import React, { Component } from "react";
+import ProgressBar from "progressbar.js";
+
+// SCSS imports
+import "./_CircleProgress.scss";
+
+class CircleProgress extends Component {
+  state = {};
+  render() {
+    return <div className="CircleProgress">xxx</div>;
+  }
+}
+
+export default CircleProgress;
