bf94898b8a23fe44b4ad324782e44bad04c07ecd
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-27 10:20:28 +0100

    Remove the border

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -12,7 +12,7 @@ import DSGNHeading from "../../components/DSGNHeading/DSGNHeading";
 import "./_Hero.scss";
 
 const Hero = () => (
-  <header className="Hero border">
+  <header className="Hero">
     <div className="container">
       <div className="row">
         <div className="col">
