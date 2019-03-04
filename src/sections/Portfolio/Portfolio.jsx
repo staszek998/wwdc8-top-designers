6c79ddad5ce1f1746f45840e049c2566767f8267
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-03-04 13:14:07 +0100

    Import more diversified images

--- src/sections/Portfolio/Portfolio.jsx
+++ src/sections/Portfolio/Portfolio.jsx
@@ -12,18 +12,113 @@ import SectionHeading from "../../components/SectionHeading/SectionHeading";
 import "./_Portfolio.scss";
 
 // Dataset.
-let dataset = [];
-for (let i = 0; i < 21; i++) {
-  dataset.push({
-    title: "Project title",
+const dataset = [
+  {
+    title: "Project Title",
     imgSrc:
-      i % 3 === 0
-        ? "https://images.unsplash.com/photo-1551527182-bf506e7484b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2026&q=80"
-        : i % 2 === 0
-        ? "https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=802&q=80"
-        : "https://images.unsplash.com/photo-1551542473-c9dc3670e2d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
-  });
-}
+      "https://images.unsplash.com/photo-1466812088935-5031240e09b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1468495244123-6c6c332eeece?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500457086163-1760e2500708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1481887328591-3e277f9473dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1518775005910-7aa25aa9614a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1527433894828-eb321c4e12fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1523467417496-ba0608c96a6a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1427751840561-9852520f8ce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1501023956373-055b874f2929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1486693326701-1ea88c6e2af3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1496427011580-872ac031756b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1470074558764-4e577e98bc85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1466812088935-5031240e09b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1458748600212-a4c65493cbfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1533621517681-fd36c2e12666?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1535222636729-76586bf52493?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1522273500616-6b4757e4c184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    title: "Project Title",
+    imgSrc:
+      "https://images.unsplash.com/photo-1524323599945-b9b2e5a44e73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  }
+];
 
 const SingleProject = ({ title, imgSrc }) => (
   <li className="SingleProject" style={{ backgroundImage: `url(${imgSrc})` }}>
