769d58c436ffffd59abc7cab89a32ffe6cb6689b
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-03-04 15:05:04 +0100

    Create the basic HTML markup

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -10,6 +10,19 @@ import React from "react";
 // SCSS imports
 import "./_Footer.scss";
 
-const Footer = () => <footer className="Footer text-center">Footer</footer>;
+const Footer = () => (
+  <footer className="Footer text-center">
+    <div className="Footer__upper">
+      <h2>CONTACT</h2>
+      <a href="mailto:free@template.com">free@template.com</a>
+      <a href="http://www.freetemplate.com">www.freetemplate.com</a>
+      <a href="tel:+48123456789">+48 123 456 789</a>
+    </div>
+
+    <div className="Footer__lower">
+      <p>Copyright &copy; DSGN | Use freely!</p>
+    </div>
+  </footer>
+);
 
 export default Footer;
